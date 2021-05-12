0e77693d695bdb73c0a1cec6afc63e4e0037fe6e
Randomguyy21 <67327168+Randomguyy21@users.noreply.github.com>
2021-05-12 12:29:30 +0530

    Add files via upload

---
+++ jss/scripts.js
@@ -0,0 +1,9 @@
+function result1()
+{ 
+    document.getElementById("book1").innerHTML= " &nbsp &nbsp <b>THIS BOOK IS ORDERED<b>";
+}
+
+function result2()
+{ 
+    document.getElementById("shoe1").innerHTML= " &nbsp &nbsp <b>THIS SHOE IS ORDERED<b>";
+}
